3f6557d1f80e11421c1482889d84926a570d2c0a
Dambaev Alexander <ice.redmine@gmail.com>
2020-10-12 03:29:44 +0000

    sync with upstream

--- derivation.nix
+++ derivation.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation {
   src = fetchgit {
     url = "https://github.com/dambaev/mqtt2csv-vicpack-payload-decoder.git";
     rev = "157c95733190e0a8f311fb97665dbfb36235c929";
-    sha256 = "aff1b2a7ad50624b8e2777b1167e189fb5b0287f";
+    sha256 = "0n079f0jb4i5rwrs9ry2g40zgx98gqh0sqil74gcn1m3rzhdx3qv";
   };
   buildInputs = with pkgs;
   [ ats2
