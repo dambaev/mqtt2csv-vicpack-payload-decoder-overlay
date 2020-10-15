497d10cf88276e7772322cc8748f7f8e04fdbb0f
Dambaev Alexander <ice.redmine@gmail.com>
2020-10-15 18:13:33 +0000

    fix hash

--- derivation.nix
+++ derivation.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation {
   src = fetchgit {
     url = "https://github.com/dambaev/mqtt2csv-vicpack-payload-decoder.git";
     rev = "7e00c4595a6ef9f00ecf9c32b1849e11c85e0b7b";
-    sha256 = "0abhm0hqc3c7zd01dci09xmixgynh0v3sgi3h1jpvivxd0j3j3hh";
+    sha256 = "1h2m1qayj9xq5106bwmpx73mfjrmcmxrv9ws2ipc32awafnygzdi";
   };
   buildInputs = with pkgs;
   [ ats2
